e2ebbb665c4f37180a9a4038a1c37d83e5d6c81a
boudfor saad <sboudfor@gmail.com>
2018-04-15 13:12:39 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 case "$1" in
-  "front --new")
-    sudo docker-compose run --rm -v /home/saad/Projects/docker-angular/apps/:/src/ node ng new $2
-    sudo chown -R $3:$4 apps/$2
+  "--front-new")
+    mkdir apps;
+    projectName=$(basename "$2");
+    echo "sudo docker-compose run --rm -v $(pwd)/apps/:/src/ node ng new $projectName"
+    sudo docker-compose run --rm -v $(pwd)/apps/:/src/ node ng new $projectName
+    sudo chown -R $3:$4 $2
+    echo "${green}Success: project $projectName was created in folder  $(pwd)/apps/:/src/"
     ;;
-  "front --serve")
+  "--front-serve")
     echo  "docker-compose run -v /home/saad/Projects/docker-angular/:/src/ --publish 4200:4200 node bash /src/script/server $2"
-    sudo docker-compose run -v /home/saad/Projects/docker-angular/apps/:/src/ --publish 4200:4200 node bash scripts/ng-server.sh $2
+    sudo docker-compose run -v  $(pwd)/apps/:/src/ --publish 4200:4200 node bash scripts/ng-server.sh $2
     ;;
-  "front --build")
+  "--front-build")
     echo "docker-compose run --rm -d -v /home/saad/Projects/docker-angular/$2:/src/$2 node ng build"
-    sudo sudo docker-compose run -v /home/saad/Projects/docker-angular/apps:/src/ --publish 4200:4200 node bash scripts/ng-build.sh $2
+    sudo sudo docker-compose run -v /$(pwd)/apps:/src/ node bash scripts/ng-build.sh $2
     ;;
   *)
     echo "You have failed to specify what to do correctly."
+    mkdir -p $2
     exit 1
     ;;
 esac
